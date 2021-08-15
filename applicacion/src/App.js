521226d9a6d51bfedffa2e2a0690457b854e312c
Jonathan <jhonn.photo@gmail.com>
2021-08-15 17:08:22 -0500

    ACreando NavBar

---
+++ applicacion/src/App.js
@@ -0,0 +1,10 @@
+import Nav from "./Nav"
+
+const App  = () => {
+    return(
+        <>
+        <Nav/>
+        </>
+    )
+}
+export default App
---
+++ applicacion/src/Nav.js
@@ -0,0 +1,19 @@
+
+
+const Nav  = () => {
+    return(
+            <header>
+            <h1>PhotoKua</h1>
+            <nav>
+               <ul>
+                   <li>Servicios</li>
+                   <li>Resultados</li>
+                   <li>Sobre Nosotros</li>
+                   <li>Contáctanos</li>
+               </ul>
+            </nav>
+        </header>
+            
+    )
+}
+export default Nav
